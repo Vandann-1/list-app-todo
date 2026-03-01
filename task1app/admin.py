4f16ce03f44b372facb020beaa9da64ce906235f
Vandann-1 <vandanparmar6041@gmail.com>
2026-03-01 10:59:31 +0530

    chnges

--- task1app/admin.py
+++ task1app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "is_completed",
+        "due_date",
+        "created_at",
+    )
+    list_filter = (
+        "is_completed",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "description",
+    )
+    ordering = ("-created_at",)
